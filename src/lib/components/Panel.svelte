fe317c4a760220a3c2d3fd66173a6e0f5443f464
waypse <slavapankov20@gmail.com>
2023-06-02 23:37:59 +0200

    initial commit

---
+++ src/lib/components/Panel.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let test = 'test';
+</script>
+
+<p>{test}</p>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Panel from '$lib/components/Panel.svelte';
+</script>
+
+<div>
+	<Panel />
+	<slot />
+</div>
+
+<style>
+</style>
